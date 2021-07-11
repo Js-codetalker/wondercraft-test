eb73090e8049d5f0d100379ca26e95a65a338bc7
CodeTalker <jcsa031403@gmail.com>
2021-07-11 12:07:10 -0500

    add JSON data created

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
+//JSON 
+const request = new XMLHttpRequest;
+request.open('GET','https://api.jsonbin.io/b/5e1aa891b236b871b3605dd6/1');
+request.send();
+
+request.addEventListener("load",()=>{
+	if (request.status == 200) {
+		let recivedData = JSON.parse(request.response);
+		var orderedData = [];
+
+		for (let i = 0; i < recivedData['data'].length; i++) {
+			if (recivedData['data'][i]['id'] && recivedData['data'][i]['name'] && recivedData['data'][i]['image'] && recivedData['data'][i]['price']) {
+				orderedData[i] = recivedData['data'][i]
+			} else {
+				console.log('Unvalid data recived');
+			}
+		}
+
+		var documentFragment = document.createDocumentFragment();
+		var productsGrid = document.querySelector('.products__grid')
+
+		for (let i = 0; i < orderedData.length; i++) {
+			let productItem = document.createElement('DIV');
+			productItem.classList.add('products__grid-item');
+			productItem.setAttribute('id',orderedData[i]['id'])
+
+			let productItemImg = document.createElement('IMG');
+			productItemImg.classList.add('products__grid-item-img');
+			productItemImg.setAttribute('src',orderedData[i]['image']);
+			productItem.appendChild(productItemImg);
+
+			let productItemDescription = document.createElement('P');
+			productItemDescription.innerHTML = orderedData[i]['name'];
+			productItemDescription.classList.add('products__grid-item-description');
+			productItem.appendChild(productItemDescription);
+
+			let productItemPrice = document.createElement('P');
+			productItemPrice.innerHTML ="$ "+(orderedData[i]['price']/100);
+			productItemPrice.classList.add('products__grid-item-price');
+			productItem.appendChild(productItemPrice);
+
+			if (orderedData[i]['best_seller']) {
+				let bestSeller = document.createElement('P');
+				bestSeller.classList.add('bestseller');
+				productItem.appendChild(bestSeller);
+			}
+			documentFragment.appendChild(productItem);
+
+			if (i == (orderedData.length-1)) {
+				productsGrid.appendChild(documentFragment)
+			}
+		}
+	}
+})
 //Modal
 let openModalButton = document.querySelector('.products__modalButton'),
 	closeModalButton = document.querySelector('.modal__head-closeButton'),
@@ -7,23 +61,23 @@ let openModalButton = document.querySelector('.products__modalButton'),
 
 function openModal() {
 	modal.classList.remove('toggle');
-}
+};
 function closeModal() {
-	modal.classList.add('toggle')
-}
+	modal.classList.add('toggle');
+};
 
 proceedButton.addEventListener('click',()=> {
 	closeModal();
-})
+});
 openModalButton.addEventListener('click',()=> {
 	openModal();
-})
+});
 closeModalButton.addEventListener('click',()=> {
 	closeModal();
-})
+});
 cancelButton.addEventListener('click',()=> {
 	closeModal();
-})
+});
 modal.addEventListener('click',()=> {
 	closeModal();
-})
+});
